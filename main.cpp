52adbe572b25289e2c94809729c0d2221931ea62
Ficoneks <filip.jambrosic09@gmail.com>
2025-01-16 17:54:03 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,33 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    int a;
-    printf("unesi prvi broj:");
-    cin>> a;
-    int b;
-    printf("unesi drugi broj:");
-    cin>>b;
-    int r = 0;
-    r= a + b;
-    printf("%i + %i = %i", a, b, r);
-    cout << endl;
-    int r1 = 0;
-    r1 = a - b;
-    printf("%i - %i = %i", a, b, r1);
-    cout << endl;
-    int r2 = 0;
-    r2 = a / b;
-    printf("%i / %i = %i", a, b, r2);
-     cout << endl;
-    int r3  = 0;
-    r3 = a * b;
-    printf("%i * %i = %i", a, b, r3);
-     cout << endl;
-
+int main(){
+    char a;
+    printf("Unsesi neki znak: ");
+    cin>>a;
+    printf(" ASCII vrijednost slova je: %i ", a);
+    cout<<endl;
+    a = a - 32;
+    printf("A njegovo veliko slovo je: %i", a);
     return 0;
-
-
 }
