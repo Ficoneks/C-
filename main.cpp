1aee723d7013f1b14de521be7ec9b5c0793516e0
Ficoneks <filip.jambrosic09@gmail.com>
2025-01-18 14:31:35 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,30 +4,10 @@ using namespace std;
 
 int main()
 {
-    int a;
-    printf("unesi prvi broj:");
+    char a;
+    printf("unesi neki znak: ");
     cin>> a;
-    int b;
-    printf("unesi drugi broj:");
-    cin>>b;
-    int r = 0;
-    r= a + b;
-    printf("%i + %i = %i", a, b, r);
-    cout << endl;
-    int r1 = 0;
-    r1 = a - b;
-    printf("%i - %i = %i", a, b, r1);
-    cout << endl;
-    int r2 = 0;
-    r2 = a / b;
-    printf("%i / %i = %i", a, b, r2);
-     cout << endl;
-    int r3  = 0;
-    r3 = a * b;
-    printf("%i * %i = %i", a, b, r3);
-     cout << endl;
-
+    a = a + 1;
+    printf("sljedeci znak je : %c", a);
     return 0;
-
-
 }
